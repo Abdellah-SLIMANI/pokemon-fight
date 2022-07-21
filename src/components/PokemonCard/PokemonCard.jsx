3bb1ce7d7687b7fe54acb69a2c34c530a55ce19d
Abdellah-SLIMANI <morscor21@gmail.com>
2022-07-21 17:31:36 +0100

    fight function halfway done

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
 import './PokemonCard.css'
+import {click} from "@testing-library/user-event/dist/click";
 
 export default function PokemonCard(props){
     const [image, setimage] = useState("")
+    const [flag, setFlag] = useState(true)
     useEffect(() => {
         axios.get(props.url)
             .then(res => {
@@ -11,8 +13,23 @@ export default function PokemonCard(props){
                 setimage(res.data.sprites.back_default)
             })
     },[])
+
+    const getIDs= () => {
+        axios.get(props.url)
+            .then(res => {
+                if(flag){
+                    props.setPokePair({...props.pokePair, poke1: res.data})
+                    setFlag(!flag)
+                }else{
+                    props.setPokePair({...props.pokePair, poke2: res.data})
+                    setFlag(flag)
+                }
+            })
+    }
+
+    console.log("pokes", props.pokePair)
     return (
-        <div className={"cardConatainer"}>
+        <div className={"cardConatainer"} onClick={()=> getIDs()}>
             <h1>{props.name}</h1>
             <img src={image} alt={props.name}/>
         </div>
--- src/containers/CardsContainer.jsx
+++ src/containers/CardsContainer.jsx
@@ -5,6 +5,8 @@ import "./CardsContainer.css"
 
 export default function CardsContainer(){
     const [pokemons, setPokemons] = useState([])
+    const [pokePair, setPokePair] = useState({poke1: "", poke2: ""})
+
     useEffect(() => {
         axios.get('https://pokeapi.co/api/v2/pokemon/')
             .then(res => {
@@ -14,11 +16,29 @@ export default function CardsContainer(){
             .catch(err => console.log("an Error happened on the api call:", err))
     },[])
 
+    const fight = (poke1 , poke2) => {
+        if(Math.floor(Math.random()*10) > 5){
+            return poke1 // TODO: axios delete function
+        }
+        return poke2 // TODO: axios delete fucntion
+    }
+
     return(
+        <>
+            {<div className={'fightsContainer'}><p>{pokePair.poke2 ? pokePair.poke1.name : ''}</p><span>VS</span>
+                <p>{pokePair.poke2 ? pokePair.poke2.name : ''}</p>
+            <button className={'fight btn'}>FIGHT</button></div>}
         <div className={'cardsContainer'}>
+            <></>
             {pokemons.sort((a,b) => a.name.localeCompare(b.name)).map(pokemon => {
-                return <PokemonCard name={pokemon.name} url={pokemon.url}/>
+                return <PokemonCard
+                            name={pokemon.name}
+                            url={pokemon.url}
+                            pokePair = {pokePair}
+                            setPokePair = {setPokePair}
+                    />
             })}
         </div>
+    </>
     )
 }
